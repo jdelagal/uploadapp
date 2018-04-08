e24638bbc6a6035642e7e5d263763f0bedf8510f
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2018-04-08 14:52:22 +0200

    res send formdata.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 
 var express = require('express');
 var app = express();
+var formdata =require('form-data');
+var fetch = require('node-fetch');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/cargar', function (req, res) {
+  
+  var form = new formdata();
+  form.append('a', 1);
+
+  res.set('Content-Type', 'multipart/form-data');
+  res.send(form);
+});
+
+// POST method route
+app.get('/guardar', function (req, res) {
+  res.send('POST request to the homepage');
 });
 
 app.listen(3000, function () {
