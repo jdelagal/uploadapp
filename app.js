666905323000f6466a187fd31e80a1a0a915a987
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2018-04-09 17:36:16 +0200

    samples write.

--- app.js
+++ app.js
@@ -2,23 +2,19 @@ var express = require('express');
 var formData = require('form-data');
 var fs = require('fs');
 var fetch = require('node-fetch');
+var stream = require('stream');
 var app = express()
 
 
 app.get('/carga', function(req, res) {
   var formData = {
     my_file: fs.createWriteStream(__dirname +'/uploads/chiquipedia.jpg')
+    //my_file: fs.createReadStream(__dirname +'/uploads/index.html')
   };
+  
   console.log('formData: ', formData ) ;
-  
-  fetch('https://datapower/factoriaustglobal/sb/jose/cargar', { method: 'POST', body: formData })
-      .then(function(res) {
-          return res.json();
-      }).then(function(json) {
-          console.log(json);
-  });
-  
-	res.send('Procesando fichero.');
+  //res.body=formData;
+	res.send(formData);
 });
 
 app.listen(3000, function () {
