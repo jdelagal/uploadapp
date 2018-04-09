f1b5a9d0f9ffd0dd429b4d6ac99830177c4440a9
Jose Joaquin De La Gala Gonzalez <jjdelagala@tcpsi.es>
2018-04-09 10:54:40 +0200

    res send formdat

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-
 var express = require('express');
-var app = express();
-var formdata =require('form-data');
+var formData = require('form-data');
+var fs = require('fs');
 var fetch = require('node-fetch');
+var app = express()
 
-app.get('/cargar', function (req, res) {
-  
-  var form = new formdata();
-  form.append('a', 1);
 
-  res.set('Content-Type', 'multipart/form-data');
-  res.send(form);
-});
 
-// POST method route
-app.get('/guardar', function (req, res) {
-  res.send('POST request to the homepage');
+var formData = {
+  my_file: fs.createWriteStream(__dirname +'/uploads/chiquipedia.jpg')
+};
+console.log('formData: ', formData ) ;
+
+fetch('https://datapower/factoriaustglobal/sb/jose/cargar', { method: 'POST', body: formData })
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        console.log(json);
 });
 
 app.listen(3000, function () {
